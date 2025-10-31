da0627429b77c184cdc80cac03a5d12b21d0ed91
João <joaokz@outlook.com>
2025-10-31 18:40:38 -0300

    feat: shadcn setup & first home page layout styles

--- web/src/pages/HomePage.tsx
+++ web/src/pages/HomePage.tsx
@@ -1,9 +1,9 @@
 
 export default function HomePage(){
     return(
-       <div>
-        <h1>Home Page</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias perspiciatis asperiores explicabo quas, ea dolor, ex modi harum provident eveniet minima temporibus praesentium corrupti alias culpa enim saepe necessitatibus? Laudantium?</p>
+       <div className="mx-auto max-w-[980px] flex flex-col gap-6 pt-8 lg:gap-8 lg:pt-[88px]">
+        <main className="flex flex-col">teste</main>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium asperiores enim consequuntur quasi dolorum possimus, numquam tempore doloremque quaerat assumenda distinctio aliquid voluptatem obcaecati perferendis quisquam quos provident repellendus! Blanditiis?</p>
        </div> 
     )
 }
