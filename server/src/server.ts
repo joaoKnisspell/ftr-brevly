8ec683f85442e487d7cd8014f825a5e4e32c1d59
João <joaokz@outlook.com>
2025-11-05 12:51:48 -0300

    fix: server initial setup adjustments

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
-import { buildApp } from "./app.ts";
+import { fastify } from "fastify";
+import { fastifyCors } from '@fastify/cors'
 
-const app = buildApp();
+const app = fastify({ logger: true });
+
+app.register(fastifyCors, {
+    origin: '*',
+})
 
 app.listen({ port: 3333 }).then(() => {
-    'HTTP Server Running...'
-}).catch((err) => {
-    console.error(err);
-})
+    console.log('🚀 Server running on port: 3333')
+}).catch((err) => console.error(err))
