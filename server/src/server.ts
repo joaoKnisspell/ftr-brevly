0bd1379ff5f5ee69c5a02aaf607b17c34d9a21f2
João <joaokz@outlook.com>
2025-11-05 13:00:23 -0300

    feat: package.json dev & build scripts & initial server app test.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,14 @@
 import { fastify } from "fastify";
 import { fastifyCors } from '@fastify/cors'
+import { linkRoutes } from "./routes/link.routes.ts";
 
 const app = fastify({ logger: true });
 
 app.register(fastifyCors, {
     origin: '*',
 })
+
+app.register(linkRoutes)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('🚀 Server running on port: 3333')
