8cdd78cf6e0133947e8647e0e261ebd2b6af33f4
João <joaokz@outlook.com>
2025-11-17 13:02:14 -0300

    feat: post, get & delete link routes.

--- server_test/src/infra/http/routes/link-routes.ts
+++ server_test/src/infra/http/routes/link-routes.ts
@@ -2,42 +2,93 @@ import { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import z from "zod";
 import { db } from "../../db";
 import { schema } from "../../db/schemas";
+import { eq } from "drizzle-orm";
 
-export const linkRoutes:FastifyPluginAsyncZod = async server => {
-    server.get("/links", (request, reply) => {
-        return reply.status(200).send({
-            data: [
-                {
-                    id: "1",
-                    original_url: "https://google.com",
-                    short_url: "https://short.com.br/1",
-                },
-                {
-                    id: "2",
-                    original_url: "https://youtube.com",
-                    short_url: "https://short.com.br/2",
-                }
-            ]
+const createLinkSchema = z.object({
+    url: z.url("URL inválida."),
+    slug: z.string().min(4).max(6),
+})
+  
+const linkResponseSchema = z.object({
+    id: z.string(),
+    url: z.string(),
+    slug: z.string(),
+})
+
+const errorSchema = z.object({
+    error: z.any(),
+})
+
+const deleteLinkParamsSchema = z.object({
+    id: z.string(),
+})
+
+
+export const linkRoutes:FastifyPluginAsyncZod = async (server) => {
+    server.post("/links",   
+        {
+            schema: {
+              summary: "Cria um link encurtado",
+              body: createLinkSchema,
+              response: {
+                201: linkResponseSchema,
+                400: errorSchema,
+                409: errorSchema,
+              },
+            },
+          }, async (request, reply) => {
+
+        const parsedBody = createLinkSchema.safeParse(request.body)
+
+        if(!parsedBody.success){
+            return reply.status(400).send({ error: parsedBody.error.flatten() })
+        }
+
+        const { url, slug } = parsedBody.data
+
+        const isLinkExists = await db.query.links.findFirst({
+            where: eq(schema.links.slug, slug)
         })
+
+        if(isLinkExists){
+            return reply.status(409).send({ error: `Link encurtado já existente. (${slug})` })
+        }
+
+        const [created] = await db.insert(schema.links).values({
+            url,
+            slug
+        }).returning()
+
+        return reply.status(201).send(created)
+
     })
 
-    server.post("/links", async (request, reply) => {
-        // const createLinkSchema = z.object({
-        //     url: z.url("URL inválida"),
-        //     slug: z.string().min(3).max(6).regex(/^[a-zA-Z0-9]+$/, 'O link encurtado pode conter apenas letras e números'),
-        // })
+    server.get("/links", {
+        schema: {
+            summary: "Busca todos os links",
+            response: {
+                200: z.array(linkResponseSchema),
+            },
+        }
+    }, async (request, reply) => {
+        const links = await db.select().from(schema.links)
 
-        // const parseResult = createLinkSchema.safeParse(request.body)
+        return reply.status(200).send(links)
+    })
 
-        // if(!parseResult.success){
-        //     return reply.status(400).send({ error: parseResult.error.flatten() })
-        // }
+    server.delete("/links/:id", {
+        schema: {
+            summary: "Deleta um link",
+            params: deleteLinkParamsSchema,
+            response: {
+                204: z.any(),
+            }
+        }
+    }, async (request, reply) => {
+        const { id } = request.params
 
-        await db.insert(schema.links).values({
-            url: 'https://google.com',
-            slug: 'google',
-        })
+        await db.delete(schema.links).where(eq(schema.links.id, id))
 
-        return reply.status(201).send({ message: 'Link criado com sucesso' })
+        return reply.status(204).send()
     })
 }
