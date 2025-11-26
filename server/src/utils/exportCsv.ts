5fc45466a2b6d21e74a0bd5a11e8a9c9ba5ab282
João <joaokz@outlook.com>
2025-11-25 21:05:47 -0300

    fix: add delimiter to csv generate function.

--- server/src/utils/exportCsv.ts
+++ server/src/utils/exportCsv.ts
@@ -6,14 +6,14 @@ type Rows = Row[];
 export function generateCsv(rows: Rows) {
   const csv = stringify(rows, {
     header: true,
-
-    columns: {
-      id: "ID",
-      slug: "Slug",
-      url: "URL Original",
-      accesses: "Cliques",
-      created_at: "Criado Em"
-    }
+    delimiter: ";",
+    columns: [
+      { key: "id", header: "ID" },
+      { key: "slug", header: "Slug" },
+      { key: "url", header: "URL Original" },
+      { key: "clicks", header: "Cliques" },
+      { key: "createdAt", header: "Criado Em" }
+    ]
   });
 
   return Buffer.from(csv, "utf-8");
